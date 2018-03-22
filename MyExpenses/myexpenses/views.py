9e1e3a88f76d9c85b397b80e7af94511d04a6bf5
mainyanim <alicejarmusch@gmail.com>
2018-03-22 11:53:47 +0200

    expenses list basic

--- MyExpenses/myexpenses/views.py
+++ MyExpenses/myexpenses/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 
 def index(request):
     num_exp = Expense.objects.all().count()
+    expenses = Expense.objects.all()
 
     return render(
         request,
         'index.html',
-        context={'num_exp': num_exp},
+        context={'num_exp': num_exp,
+                 'expenses': expenses},
     )
